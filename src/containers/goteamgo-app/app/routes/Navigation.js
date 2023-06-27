b6f1999d82c86937ac361f6ca1b649909c0f14a1
ilès <iles@mentorgoal.com>
2023-06-27 15:04:22 +0200

    rebase

--- src/containers/goteamgo-app/app/routes/Navigation.js
+++ src/containers/goteamgo-app/app/routes/Navigation.js
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-=======
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-import GoAccess from '../screens/GoAccess';
->>>>>>> 9812305 (login navigation)
 import GoLogin from '../screens/GoLogin'
 import GoSignup from '../screens/GoSignup'
 
 const Stack = createNativeStackNavigator();
 
-<<<<<<< HEAD
 export default function Navigation() {
     return (
         <Stack.Navigator initialRouteName="GoLogin" screenOptions={{ headerShown: false }}>
@@ -21,18 +14,3 @@ export default function Navigation() {
         </Stack.Navigator>
     );
 }
-=======
-export default function AcessStack() {
-
-    return (
-        <Stack.Navigator screenOptions={{ headerShown: true }}>
-            <Stack.Screen name="Access" component={GoAccess} />
-            <Stack.Screen name="GoLogin" component={GoLogin} />
-            <Stack.Screen name="GoSignup" component={GoSignup} />
-        </Stack.Navigator>
-
-    )
-
-}
-
->>>>>>> 9812305 (login navigation)
