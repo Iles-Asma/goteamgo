366f96e792193a649bc3d46a7f31e6562408e0e1
benribou <ribou.benjamin@gmail.com>
2023-06-27 14:59:06 +0200

    Vue inscription créer

--- src/containers/goteamgo-app/app/screens/GoSignup.jsx
+++ src/containers/goteamgo-app/app/screens/GoSignup.jsx
@@ -91,8 +91,8 @@ export default function GoSignup({ navigation }) {
 
 				{/* <GoButtonOutlined btnTxt="Connexion" onPress={() => props.navigation.goBack()} /> */}
 				<GoButtonOutlined btnTxt="Connexion" onPress={() => navigation.navigate('GoLogin')} />
-			</View>
-		</SafeAreaView>
+			</View >
+		</SafeAreaView >
 	);
 }
 
