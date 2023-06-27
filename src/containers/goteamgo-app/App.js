981230518339277aa311b9d51a03e6f6cc5f6cc5
ilès <iles@mentorgoal.com>
2023-06-27 14:56:21 +0200

    login navigation

--- src/containers/goteamgo-app/App.js
+++ src/containers/goteamgo-app/App.js
@@ -1,35 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
-import GoAccess from './app/screens/GoAccess';
-import GoLogin from './app/screens/GoLogin';
-import GoButton from './app/components/GoButton';
-import GoButtonOutlined from './app/components/GoButtonOutlined';
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigation from './app/routes/Navigation'
 
 export default function App() {
 
   return (
-    <View style={styles.container}>
-      <GoAccess />
-    </View>
+
+    <NavigationContainer>
+      <Navigation />
+    </NavigationContainer>
+
   );
 
 }
 
-const styles = StyleSheet.create({
-  touchable: {
-    borderRadius: 15,
-    width: 295,
-    height: 61,
-    backgroundColor: "#79BFFF",
-    marginBottom: 15,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btnSpace: {
-    paddingBottom: 10,
 
-  }
-});
---
+++ src/containers/goteamgo-app/app/routes/Navigation.js
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import GoAccess from '../screens/GoAccess';
+import GoLogin from '../screens/GoLogin'
+import GoSignup from '../screens/GoSignup'
+
+const Stack = createNativeStackNavigator();
+
+export default function AcessStack() {
+
+    return (
+        <Stack.Navigator screenOptions={{ headerShown: true }}>
+            <Stack.Screen name="Access" component={GoAccess} />
+            <Stack.Screen name="GoLogin" component={GoLogin} />
+            <Stack.Screen name="GoSignup" component={GoSignup} />
+        </Stack.Navigator>
+
+    )
+
+}
+
