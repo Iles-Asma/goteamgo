09bee03f1920f9d3f3dd44aaebb3ccf29aad279b
matteo <matteo.de-lutiis@@edu.esiee-it.fr>
2023-06-27 15:16:20 +0200

    Ajout des champs texte input dans la vue profil

--- src/containers/goteamgo-app/app/screens/GoProfil.jsx
+++ src/containers/goteamgo-app/app/screens/GoProfil.jsx
@@ -1,7 +1,15 @@
 import { StyleSheet, Text, View, StatusBar, SafeAreaView, Platform } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
+import GoTextInput from '../components/GoTextInput'
 
 export default function GoProfil() {
+
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [organisation] = useState('');
+
   return (
     <SafeAreaView style={styles.containers}>
       <StatusBar style="auto" />
@@ -9,10 +17,31 @@ export default function GoProfil() {
       <Text>Votre Profil</Text>
 
       <Text>Nom</Text>
+      <GoTextInput
+        value={nom}
+        onChangeText={setNom} />
+
       <Text>Prénom</Text>
+      <GoTextInput
+        value={prenom}
+        onChangeText={setPrenom} />
+
       <Text>Email</Text>
+      <GoTextInput
+        value={email}
+        onChangeText={setEmail} />
+
       <Text>Mot de passe</Text>
+      <GoTextInput
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry />
+
       <Text>Rejoindre une organisation</Text>
+      <GoTextInput
+        value={organisation}
+        onChangeText={setPassword} />
+
       <Text>Mes organisations</Text>
 
     </SafeAreaView>
