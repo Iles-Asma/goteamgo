fac792df921c33e2a62f6fe63abe9469a0547947
matteo <matteo.de-lutiis@@edu.esiee-it.fr>
2023-06-27 14:36:21 +0200

    Création de la vue Profil avec les champs textes

--- src/containers/goteamgo-app/app/screens/GoProfil.jsx
+++ src/containers/goteamgo-app/app/screens/GoProfil.jsx
@@ -1,30 +1,8 @@
-import { StyleSheet, Text, View, StatusBar, SafeAreaView, Platform } from 'react-native'
+import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function GoProfil() {
-
-  const [nom, setNom] = useState('');
-  const [prenom, setPrenom] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [organisation, setOrganisation] = useState([]);
-
-  const handleLogin = () => {
-    // Fonction de gestion de la connexion ici
-    console.log('Nom:', nom);
-    console.log('Prenom:', prenom);
-    console.log('Email:', email);
-    console.log('Mot de passe:', password);
-
-    alert("Votre profil a été mis à jour");
-  };
-
-  const renderOrganisation = ({ item }) => (
-    <View>
-      <Text>{item.name}</Text>
-    </View>
-  );
-
   return (
     <SafeAreaView style={styles.containers}>
       <StatusBar style="auto" />
@@ -37,12 +15,6 @@ export default function GoProfil() {
       <Text>Mot de passe</Text>
       <Text>Rejoindre une organisation</Text>
       <Text>Mes organisations</Text>
-
-      <FlatList
-        data={organisation}
-        renderItem={renderOrganisation}
-        keyExtractor={(item) => item.id.toString()}
-      />
 
     </SafeAreaView>
   )
