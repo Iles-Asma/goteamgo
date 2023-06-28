f65d5018201499a7dee0709899a3441d2baf79ea
ilès <iles@mentorgoal.com>
2023-06-28 15:42:44 +0200

    conflict

--- src/containers/goteamgo-app/app/screens/GoProfil.jsx
+++ src/containers/goteamgo-app/app/screens/GoProfil.jsx
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View, StatusBar, SafeAreaView, Platform } from 'react-native'
 import React, { useState } from 'react'
 import GoTextInput from '../components/GoTextInput'
+import GoButton from '../components/GoButton';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function GoProfil() {
 
@@ -8,10 +10,26 @@ export default function GoProfil() {
   const [prenom, setPrenom] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [organisation] = useState('');
+  const [organisation, setOrganisation] = useState([]);
+
+  const handleLogin = () => {
+    // Fonction de gestion de la connexion ici
+    console.log('Nom:', nom);
+    console.log('Prenom:', prenom);
+    console.log('Email:', email);
+    console.log('Mot de passe:', password);
+
+    alert("Votre profil a été mis à jour");
+  };
+
+  const renderOrganisation = ({ item }) => (
+    <View>
+      <Text>{item.name}</Text>
+    </View>
+  );
 
   return (
-    <SafeAreaView style={styles.containers}>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
 
       <Text>Votre Profil</Text>
@@ -37,12 +55,23 @@ export default function GoProfil() {
         onChangeText={setPassword}
         secureTextEntry />
 
+      <View style={styles.btnEspace}>
+        <GoButton onPress={handleLogin} btnTxt="Modifier votre profil" />
+      </View>
+
       <Text>Rejoindre une organisation</Text>
       <GoTextInput
         value={organisation}
         onChangeText={setPassword} />
+      <GoButton onPress={handleLogin} btnTxt="OK" />
 
       <Text>Mes organisations</Text>
+
+      <FlatList
+        data={organisation}
+        renderItem={renderOrganisation}
+        keyExtractor={(item) => item.id.toString()}
+      />
 
     </SafeAreaView>
   )
@@ -57,4 +86,8 @@ const styles = StyleSheet.create({
     backgroundColor: "#ffffff",
     gap: 10
   },
+
+  btnEspace: {
+    gap: 10,
+  }
 })
