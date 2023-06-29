a9cfadc00dfe8a5f9189246e26055673cf65164c
benribou <ribou.benjamin@gmail.com>
2023-06-29 14:41:35 +0200

    Ajout de la connexion et redirection vers la home

--- src/containers/goteamgo-app/app/routes/Navigation.js
+++ src/containers/goteamgo-app/app/routes/Navigation.js
@@ -1,37 +1,33 @@
+// Navigation.js
+
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
-import GoLogin from '../screens/GoLogin'
-import GoSignup from '../screens/GoSignup'
+import GoLogin from '../screens/GoLogin';
+import GoSignup from '../screens/GoSignup';
 import GoProfil from '../screens/GoProfil';
 
 const Stack = createNativeStackNavigator();
-
-export default function Navigation() {
-    return (
-        <Stack.Navigator initialRouteName="GoLogin" screenOptions={{ headerShown: false }}>
-            <Stack.Screen name="GoLogin" component={GoLogin} />
-            <Stack.Screen name="GoSignup" component={GoSignup} />
-        </Stack.Navigator>
-    );
-}
-
 const Tab = createBottomTabNavigator();
-export function Navigator() {
 
-
+function BottomTabs() {
     return (
         <Tab.Navigator>
-            {/*  Affiche l'ecran d'accueil */}
+            {/* Affiche l'écran d'accueil */}
             <Tab.Screen name="GoSignup" component={GoSignup} />
 
-            {/* affiche l'ecrande profil */}
+            {/* Affiche l'écran de profil */}
             <Tab.Screen name="GoProfil" component={GoProfil} />
         </Tab.Navigator>
     );
 }
 
-
-
+export default function Navigation() {
+    return (
+        <Stack.Navigator initialRouteName="GoLogin" screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="GoLogin" component={GoLogin} />
+            <Stack.Screen name="Home" component={BottomTabs} />
+        </Stack.Navigator>
+    );
+}
