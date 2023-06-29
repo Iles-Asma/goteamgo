6302c749d4a17e1257593d235ffb15e8227e6263
matteo <matteo.de-lutiis@@edu.esiee-it.fr>
2023-06-29 16:17:16 +0200

    #10 Ajout du component GoViewVoiture

---
+++ src/containers/goteamgo-app/app/components/GoViewVoiture.jsx
@@ -0,0 +1,61 @@
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+
+export default function GoViewVoiture(props) {
+  return (
+    <TouchableOpacity style={styles.touchable} onPress={props.onPress}>
+			<View style={styles.btnContainer}>
+                <View style={styles.icone}>
+                    <MaterialCommunityIcons name="car-side" color="#79BFFF" size={50}/>
+                </View>
+                <View style={styles.textes}>
+                    <Text style={styles.nomTxt}>{props.nomTxt}</Text>
+                    <Text style={styles.placeTxt}>{props.placeTxt}</Text>
+                </View>
+                <View >
+                    <MaterialCommunityIcons name="checkbox-blank-circle" color="#63CA23" size={15}/>
+                </View>
+			</View>
+	</TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+	touchable: {
+		borderRadius: 15,
+		width: 325,
+		height: 61,
+		backgroundColor: "#F2F2F2",
+	},
+	nomTxt: {
+        fontWeight: 'bold',
+		textAlignVertical: "center",
+		textAlign: "center",
+		color: "#000000",
+	},
+
+    placeTxt: {
+        fontWeight: 'bold',
+        textAlignVertical: "center",
+		textAlign: "center",
+        color: "#63CA23",
+    },
+
+	btnContainer: {
+		flex: 1,
+        flexDirection: "row",
+		alignItems: "center",
+        justifyContent: "space-around",
+        padding: 20,
+	},
+
+    icone: {
+        width: "auto"
+    },
+
+    textes: {
+        alignItems: "flex-start",
+        padding : 10,
+    },
+});
