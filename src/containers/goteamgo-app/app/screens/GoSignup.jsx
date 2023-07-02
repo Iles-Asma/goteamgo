d3ccfc05c32d16c4b5be80166cb1278d2b05ff37
benribou <ribou.benjamin@gmail.com>
2023-07-03 00:40:00 +0200

    correction bug logo

--- src/containers/goteamgo-app/app/screens/GoSignup.jsx
+++ src/containers/goteamgo-app/app/screens/GoSignup.jsx
@@ -71,11 +71,6 @@ export default function GoSignup({ navigation }) {
         <SafeAreaView style={styles.container}>
             <StatusBar style="auto" />
 
-            <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={styles.keyboardAvoidingView}
-    >
-
             <Logo style={{ marginTop: 85 }} />
 
             <Text style={styles.titre}>Welcome !</Text>
@@ -109,7 +104,6 @@ export default function GoSignup({ navigation }) {
                 <GoButton onPress={handleSignup} btnTxt="Inscription" />
                 <GoButtonOutlined btnTxt="Connexion" onPress={() => navigation.navigate('GoLogin')} />
             </View>
-            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
